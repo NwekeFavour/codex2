83a7f1db58731a454afd79e4f5248b907a667629
NwekeFavour <buchijunior90@gmail.com>
2025-09-26 20:52:26 +0100

    added auth and form collection feature

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose")
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
-  } catch (error) {
-    console.error(`Error: ${error.message}`)
-    process.exit(1)
-  }
+    try {   
+        mongoose.set('strictQuery', false);
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Database is connected successfully: ${connect.connection.host}`);
+
+        // await createAdmin();
+    } catch (e) {
+        console.error(e);   
+    }
 }
 
 module.exports = connectDB
